1ac3212370a10dfb1f6d91eb3231cc28894e2b8a
luigicfilho <67793599+luigicfilho@users.noreply.github.com>
2025-02-15 11:01:31 +0000

    removing migrations folder and uncomment some stuff on the dbcontext (it's not working, probably because fields don't exist)

--- LCFila.Infra/Context/FilaDbContext.cs
+++ LCFila.Infra/Context/FilaDbContext.cs
@@ -32,23 +32,25 @@ public class FilaDbContext : IdentityDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //foreach (var property in modelBuilder.Model.GetEntityTypes()
-        //    .SelectMany(e => e.GetProperties()
-        //        .Where(p => p.ClrType == typeof(string))))
-        //    property.SetColumnType("varchar(100)");
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+            property.SetColumnType("varchar(100)");
 
-        //modelBuilder.ApplyConfigurationsFromAssembly();
-        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FilaDbContext).Assembly);
+
         foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.Cascade;
 
         base.OnModelCreating(modelBuilder);
+        // SQLite don't support multiple Schemas
         //modelBuilder.HasDefaultSchema("Identity");
         //modelBuilder.Entity<ApplicationUser>().ToTable("AspNetUsers", "security");
-
 
         modelBuilder.Entity<AppUser>()
             .Property(u => u.Id)
             .HasMaxLength(128); // Let SQLite auto-generate it
+       
+        // SQLite don't support multiple Schemas
         ////modelBuilder.Entity<IdentityUser>(entity =>
         ////{
         ////    entity.ToTable(name: "User", "Identity");
@@ -81,18 +83,18 @@ public class FilaDbContext : IdentityDbContext
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
     {
-        //foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
-        //{
-        //    if (entry.State == EntityState.Added)
-        //    {
-        //        entry.Property("DataCadastro").CurrentValue = DateTime.Now;
-        //    }
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+            }
 
-        //    if (entry.State == EntityState.Modified)
-        //    {
-        //        entry.Property("DataCadastro").IsModified = false;
-        //    }
-        //}
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("DataCadastro").IsModified = false;
+            }
+        }
 
         return base.SaveChangesAsync(cancellationToken);
     }
